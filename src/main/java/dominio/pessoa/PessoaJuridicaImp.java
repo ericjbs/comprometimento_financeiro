6e5d8f0ddb406905d18487ee783e436a6fa820a7
Eric Silva <ext.ebasiliodasilva@riotgames.com>
2023-02-02 14:26:30 -0300

    Todos os testes passando

--- src/main/java/dominio/pessoa/PessoaJuridicaImp.java
+++ src/main/java/dominio/pessoa/PessoaJuridicaImp.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import dominio.bemImovel.BemImovel;
 import dominio.estruturaSocietaria.EstruturaSocietaria;
+import dominio.estruturaSocietaria.EstruturaSocietariaImp;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,21 @@ import lombok.Setter;
 public class PessoaJuridicaImp implements PessoaJuridica{
     List<BemImovel> bens;
     EstruturaSocietaria estruturaSocietaria;
+
+    public EstruturaSocietaria getEstruturaSocietaria(){
+        // TODO: Isso cria uma inconsistencia no método, o requisito deve ser mudado para aceitar a empresa como parametro
+        // Caso a empresa não seja socia dela mesma, para considerar seus bens, adiciono ela mesma na estrutura societaria
+        if(estruturaSocietaria.getSocios().contains(this)) {
+            return estruturaSocietaria;
+        } else {
+            var novaEstrutura = new EstruturaSocietariaImp();
+            var novosSocios = new ArrayList<Pessoa>();
+            novosSocios.addAll(estruturaSocietaria.getSocios());
+            novosSocios.add(this);
+            novaEstrutura.setSocios(novosSocios);
+            return novaEstrutura;
+        }
+    }
 
     public PessoaJuridicaImp(){
         this.bens = new ArrayList<>();
