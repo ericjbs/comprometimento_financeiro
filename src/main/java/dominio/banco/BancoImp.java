8b002b72b7e81bf933e302fac8aec5adb9a3e9fb
Eric Silva <ext.ebasiliodasilva@riotgames.com>
2023-02-07 22:27:27 -0300

    Refatora

--- src/main/java/dominio/banco/BancoImp.java
+++ src/main/java/dominio/banco/BancoImp.java
@@ -12,42 +12,41 @@ import dominio.pessoa.PessoaJuridica;
 public class BancoImp implements Banco {
     @Override
     public BigDecimal comprometimentoFinanceiro(EstruturaSocietaria estrutura) {
-        ComprometimentoFinanceiro comprometimento = new ComprometimentoFinanceiro();
-        comprometimento.pessoasConsideradas = new ArrayList<>();
+        Comprometimento comprometimento = new Comprometimento();
+        comprometimento.pessoas = new ArrayList<>();
         comprometimento.valor = (BigDecimal.ZERO);
 
-        this.processaComprometimentoFinanceiro(comprometimento, estrutura);
+        this.processa(comprometimento, estrutura);
 
         return comprometimento.valor;
     }
 
-    private ComprometimentoFinanceiro processaComprometimentoFinanceiro(ComprometimentoFinanceiro comprometimento,
+    private Comprometimento processa(Comprometimento comprometimento,
         EstruturaSocietaria estrutura) {
 
         for (int i = 0; i < estrutura.getSocios().size(); i++) {
             var socio = estrutura.getSocios().get(i);
 
-            if (comprometimento.pessoasConsideradas.contains(socio)) continue;
+            if (comprometimento.pessoas.contains(socio)) continue;
 
-            comprometimento.pessoasConsideradas.add(socio);
+            comprometimento.pessoas.add(socio);
 
             // Se for uma pessoa juridica, percorre a estrutura societaria desse sócio, recursivamente, e acresce o valor do comprometimento
             if (socio instanceof PessoaJuridica) {
-                comprometimento.valor = processaComprometimentoFinanceiro(comprometimento,
-                        ((PessoaJuridica) socio).getEstruturaSocietaria()).valor
-                    .add(socio.getBens().stream().map(BemImovel::getValor).reduce(BigDecimal.ZERO, BigDecimal::add));
+                comprometimento.valor = processa(comprometimento, ((PessoaJuridica) socio).getEstruturaSocietaria()).valor
+                        .add(socio.getBens().stream().map(BemImovel::getValor).reduce(BigDecimal.ZERO, BigDecimal::add));
             } else {
                 // Se for pessoa física, não há estrutura societária, portanto não há recursividade
-                comprometimento.valor = comprometimento.valor.add(socio.getBens().stream()
-                        .map(BemImovel::getValor).reduce(BigDecimal.ZERO, BigDecimal::add));
+                comprometimento.valor = comprometimento.valor
+                    .add(socio.getBens().stream().map(BemImovel::getValor).reduce(BigDecimal.ZERO, BigDecimal::add));
             }
         }
 
         return comprometimento;
     }
 
-    private static class ComprometimentoFinanceiro {
+    private static class Comprometimento {
         private BigDecimal valor;
-        private List<Pessoa> pessoasConsideradas;
+        private List<Pessoa> pessoas;
     }
 }
